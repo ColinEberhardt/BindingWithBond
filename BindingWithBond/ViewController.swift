81828a02389e6e1960380dec61dcf100d9b33ab0
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-12-10 08:39:20 +0000

    Initial Commit

---
+++ BindingWithBond/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BindingWithBond
+//
+//  Created by Colin Eberhardt on 10/12/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
