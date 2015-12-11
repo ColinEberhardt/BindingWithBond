87ae127f3ff9a74382bf8d83f5034bb2113dbaa5
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-12-11 17:48:57 +0000

    Date range constraints

--- BindingWithBond/ViewModel/PhotoSearchMetadataViewModel.swift
+++ BindingWithBond/ViewModel/PhotoSearchMetadataViewModel.swift
@@ -14,4 +14,21 @@ class PhotoSearchMetadataViewModel {
   let dateFilter = Observable<Bool>(false)
   let minUploadDate = Observable<NSDate>(NSDate())
   let maxUploadDate = Observable<NSDate>(NSDate())
+  
+  init() {
+    maxUploadDate.observe {
+      [unowned self]
+      maxDate in
+      if maxDate.timeIntervalSinceDate(self.minUploadDate.value) < 0 {
+        self.minUploadDate.value = maxDate
+      }
+    }
+    minUploadDate.observe {
+      [unowned self]
+      minDate in
+      if minDate.timeIntervalSinceDate(self.maxUploadDate.value) > 0 {
+        self.maxUploadDate.value = minDate
+      }
+    }
+  }
 }
